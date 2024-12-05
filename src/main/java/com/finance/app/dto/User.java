ae32e5bfbf5f23768b9df57299d619aeb891b855
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2024-12-05 19:26:52 +0530

    Init Commit and add User DTO

---
+++ src/main/java/com/finance/app/dto/User.java
@@ -0,0 +1,40 @@
+package com.finance.app.dto;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role = "USER";
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "created_date", nullable = false)
+    private String createdDate;
+
+    @Column(name = "updated_date", nullable = false)
+    private String updatedDate;
+}
+
